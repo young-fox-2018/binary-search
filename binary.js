42836652319d3a82d4bf85e8cbabc5e0ead27e32
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-10-30 17:08:59 +0700

    done

--- binary.js
+++ binary.js
@@ -2,15 +2,45 @@
 
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
+// var testArrayGenap = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]
 
 function ownSort(arr) {
-  // Your sorting code
+  for (let i = 1; i < arr.length; i++) {
+    debugger
+    let temp = 0
+    debugger
+    for (let j = i; j > 0; j--) {
+      debugger
+      if (arr[j] < arr[j - 1]) {
+        debugger
+        temp = arr[j]
+        arr[j] = arr[j - 1]
+        arr[j - 1] = temp
+        debugger
+      }
+    }
+  }
+  debugger
   return arr
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search(search, array) {
+  let low = 0
+  let height = array.length - 1
+  while (low <= height) {
+    let mid = Math.floor((low + height) / 2)
+    if (search === array[mid]) {
+      return mid
+    }
+    else if (search > array[mid]) {
+      low = mid + 1
+    }
+    else {
+      height = mid - 1
+    }
+
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
