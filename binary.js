de3fc7eaedcacf12fa5d504b41f0ff7b1734e241
Desy Armariena <desy.armariena@gmail.com>
2018-10-30 17:03:07 +0700

    Binary search with insertion sorting algorithm

--- binary.js
+++ binary.js
@@ -5,12 +5,50 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
+  for (var i = 1; i < arr.length; i++) {
+
+    var pembanding = arr[i];
+    //cek mundur
+    var j = i - 1;
+    while (j >= 0 && arr[j] > pembanding) {
+        // console.log(arr[j], arr[i], i, j);
+        var temp = arr[j];            
+        arr[j+1] = arr[j];
+        arr[j] = pembanding
+        // console.log(arr[j], arr[i]);
+        j--;
+    }
+  }
+
+  return arr;
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  var indexAwal = 0;
+    var indexAkhir = array.length - 1;
+    var indexTengah = 0;
+    
+    var found = false;
+    while (!found && indexAkhir >= indexAwal) {
+        indexTengah = Math.floor((indexAkhir - indexAwal)/2) + indexAwal;
+
+        if (array[indexTengah] === search) {
+            found = true;
+        }
+        if (array[indexTengah] > search) {
+            indexAkhir = indexTengah - 1;
+        } else {
+            indexAwal = indexTengah + 1;
+        }
+        debugger
+    }
+
+    if (found) {
+        return indexTengah;
+    }
+
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
@@ -24,6 +62,18 @@ console.log(binary_search(33, arrayGenapSorted))
 console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
 console.log(binary_search(2, arrayGanjilSorted))
+
+let test_array = [1, 2, 3, 4, 5];
+console.log(binary_search(3, test_array) === 2); // true
+
+test_array = [13, 19, 24, 29, 32, 37, 43];
+console.log(binary_search(35, test_array) === -1); // true
+
+test_array = [100, 120, 130, 135, 150, 170];
+console.log(binary_search(135, test_array) === 3); // true
+
+console.log(binary_search(32, [13, 19, 24, 29, 32, 37, 43]));
+
 
 module.exports = {
   binary_search
