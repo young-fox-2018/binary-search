0608ba1d081f78cc9221f1614b95cdbcc96254e0
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-10-30 15:34:58 +0700

    tugas

--- binary.js
+++ binary.js
@@ -1,20 +1,59 @@
-'use strict'
+// 'use strict'
 
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
+  // var arr = arr.sort(function(a,b){return a-b})
+  // var index = 0 
+  var angka = 0
+  var j = 0 
+
+  for(var i = 0 ; i < arr.length ; i++){
+    angka = arr[i];
+    j = i-1;
+
+    // PERSYARATAN BIAR BERHENTINYA :
+    while(j >=0 && arr[j] > angka){
+      arr[j+1] = arr[j]; 
+      j = j-1;
+    }
+
+    arr[j+1] = angka
+  }
+
   return arr
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+
+function binary_search (search, array) {
+
+  var awal = 0 ; 
+  var akhir = array.length-1
+  var tengah = Math.round((awal + akhir)/2)
+  // console.log(tengah)
+
+  while(akhir >= awal){
+    debugger
+    if (array[tengah] === search){
+      return tengah
+    } else if (search < array[tengah] ){
+      akhir = tengah - 1
+    } else {
+      awal = tengah + 1
+    }
+    tengah =  Math.floor((awal + akhir)/2)
+    debugger
+    // console.log(awal, tengah, akhir)
+  }
+
+  return -1
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
+console.log(arrayGenapSorted)
+console.log(arrayGanjilSorted)
 
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
