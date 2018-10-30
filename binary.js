2139a937cce420c79c4feedcbe87bd70a1d3ecd8
Atras Tudhi <atras.r@gmail.com>
2018-10-30 17:37:04 +0700

    binary search challange done

--- binary.js
+++ binary.js
@@ -5,17 +5,55 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  for(let i = 0; i < arr.length; i++) {
+    let index = i
+
+    if(index !== 0) {
+      for(let j = index; j > 0; j--) {
+        let temp
+        if(arr[index] < arr[index-1]) {
+          temp = arr[index]
+          arr[index] = arr[index-1]
+          arr[index-1] = temp
+        }
+        index = index - 1      
+      }
+    }
+
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  let low = 0
+  let high = array.length
+  let mid = Math.floor((low + high) / 2)
+
+  while(array[mid] !== search) {
+      
+      if(low === high) {
+          return -1
+        }
+    
+      if(array[mid] < search) {
+        low = mid + 1
+      } else {
+        high = mid - 1
+      }
+      
+      mid = Math.floor((low + high) / 2)
+      
+  }
+
+  return mid
+  // return -1
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
+// console.log(arrayGenapSorted)
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
