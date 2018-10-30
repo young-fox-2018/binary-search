49b75f4ac712ee24bd5918c541b13b13dcf12def
Cascade <mufhabu@gmail.com>
2018-10-30 17:38:25 +0700

    done binary-search

--- binary.js
+++ binary.js
@@ -5,12 +5,36 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  let temp;
+  for (let i = 0; i < arr.length; i++) {
+    let k = i;
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[k])
+        k = j;
+    }
+    temp = arr[i];
+    arr[i] = arr[k];
+    arr[k] = temp;
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search(search, array) {
   // Your searching code
-  return 0;
+  let right = array.length - 1
+  let left = 0
+  while (right >= left) {
+    let middle = Math.floor((right + left) / 2)
+    let mid = array[middle]
+    if (mid == search) {
+      return middle
+    } else if (search > mid) {
+      left = left + 1
+    } else {
+      right = middle - 1
+    }
+  }
+  return -1
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
